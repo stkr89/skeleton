24333ab4404a21b23f8da9e9b50365a279c1f7bb
sumittokkar <OOswvwnnn@007>
2020-12-17 06:44:14 -0600

    added comments

--- request.go
+++ request.go
@@ -1,5 +1,6 @@
 package skeleton
 
+// Request specifies general fields required to make an http call
 type Request struct {
 	Url     string
 	Method  string
@@ -8,17 +9,20 @@ type Request struct {
 	Auth    *Auth
 }
 
+// Auth contains pointers to the type of authentications supported by skeleton
 type Auth struct {
 	Basic       *AuthBasic
 	BearerToken *AuthBearerToken
 	Custom      map[string]string
 }
 
+// AuthBasic provides fields required for http basic authentication
 type AuthBasic struct {
 	Username string
 	Password string
 }
 
+// AuthBearerToken provides field required for http token based authentication
 type AuthBearerToken struct {
 	Token string
 }
